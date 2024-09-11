3b2ce12037437d53d6eda1679ff32f56e35dd429
opickers90 <taufik.irfan90@gmail.com>
2024-09-11 13:51:18 +0700

    first

---
+++ latihan1.py
@@ -0,0 +1,10 @@
+# Tulis program di sini
+a = int(input(angka pertama:))
+b = int(input(angka kedua:))
+
+print(a, b)
+
+
+# Masukkan angka pertama: 3
+# Masukkan angka kedua: 4
+# Output: Hasil penjumlahan: 7
